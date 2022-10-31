d9ffece4c36914a6876b640f76eb0813f31ac72e
Daniel DeGroff <daniel@fusionauth.io>
2022-10-30 19:25:40 -0600

    domain sync

--- src/FusionAuth/FusionAuthClient.php
+++ src/FusionAuth/FusionAuthClient.php
@@ -240,9 +240,9 @@ class FusionAuthClient
   /**
    * Make a Client Credentials grant request to obtain an access token.
    *
-   * @param string $client_id The client identifier. The client Id is the Id of the FusionAuth Entity in which you you are attempting to authenticate.
+   * @param string $client_id The client identifier. The client Id is the Id of the FusionAuth Entity in which you are attempting to authenticate.
    * @param string $client_secret The client secret used to authenticate this request.
-   * @param string $scope (Optional) This parameter is used to indicate which target entity you are requesting access. To request access to an entity, use the format target-entity:<target-entity-id>:<roles>. Roles are an optional comma separated list.
+   * @param string $scope (Optional) This parameter is used to indicate which target entity you are requesting access. To request access to an entity, use the format target-entity:&lt;target-entity-id&gt;:&lt;roles&gt;. Roles are an optional comma separated list.
    *
    * @return ClientResponse The ClientResponse.
    * @throws \Exception
